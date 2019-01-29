d5e88ba8e472e55737d0fe51efc06167be34b1dd
Ramassamy Lucien <Lucien@Lucien.local>
2019-01-29 20:10:02 +0100

    changement des tps d'animation et ajout d'effets

--- js/app.js
+++ js/app.js
@@ -1,105 +1,100 @@
 $('document').ready(function () {
 
-    $(".fr").mouseover(function () {
+    $('.fr').mouseover(function () {
 
-        TweenMax.to('.france', 0.7, {
-
-            width: "80%",
-
+        TweenMax.to('.image', 0.2, {
+            width: 0,
             onComplete: function () {
 
-                $(".fr").mouseout(function () {
-
-                    TweenMax.to('.france', 0.1, {
-
-                        width: "0"
-
-                    });
+                TweenMax.to('.france', 1, {
+                    width: '80%',
+                    ease: Back.easeOut.config(1.7),
                 });
             }
-            
         });
     });
 
+    $('.es').mouseover(function () {
 
-    // ----- //
-
-
-
-    $(".es").mouseover(function () {
-
-        TweenMax.to('.espagne', 0.7, {
-
-            width: "80%",
+        TweenMax.to('.image', 0.2, {
+            width: 0,
             onComplete: function () {
 
-
-                $(".es").mouseout(function () {
-
-                    TweenMax.to('.espagne', 0.1, {
-
-                        width: "0"
-
-                    });
+                TweenMax.to('.espagne', 1, {
+                    width: '80%',
+                    ease: Back.easeOut.config(1.7),
                 });
             }
-
         });
     });
 
+    $('.al').mouseover(function () {
 
-
-    // ----- //
-
-
-
-    $(".po").mouseover(function () {
-
-        TweenMax.to('.portugal', 0.7, {
-
-            width: "80%",
+        TweenMax.to('.image', 0.2, {
+            width: 0,
             onComplete: function () {
 
-
-                $(".po").mouseout(function () {
-
-                    TweenMax.to('.portugal', 0.1, {
-
-                        width: "0"
-
-                    });
+                TweenMax.to('.allemagne', 1, {
+                    width: '80%',
+                    ease: Back.easeOut.config(1.7),
                 });
             }
-
         });
     });
 
+    $('.po').mouseover(function () {
 
-
-    // ----- //
-
-
-
-    $(".al").mouseover(function () {
-
-        TweenMax.to('.allemagne', 0.7, {
-
-            width: "80%",
+        TweenMax.to('.image', 0.2, {
+            width: 0,
             onComplete: function () {
 
-
-                $(".al").mouseout(function () {
-
-                    TweenMax.to('.allemagne', 0.1, {
-
-                        width: "0"
-
-                    });
+                TweenMax.to('.portugal', 1, {
+                    width: '80%',
+                    ease: Back.easeOut.config(1.7),
                 });
             }
-
         });
     });
+
+    // ----- ----- ----- ----- ----- ----- ----- //
+
+
+    TweenMax.from('.fr', 1, {
+        css: {
+            opacity: 0,
+            y: "50%"
+        }
+    });
+    
+
+    TweenMax.from('.es', 1, {
+        css: {
+            opacity: 0,
+            y: "50%"
+        }, delay: 0.1
+    });
+    
+
+    TweenMax.from('.po', 1, {
+        css: {
+            opacity: 0,
+            y: "50%"
+        }, delay: 0.2
+    });
+    
+
+    TweenMax.from('.al', 1, {
+        css: {
+            opacity: 0,
+            y: "50%"
+        }, delay: 0.3
+    });
+    
+
+
+
+
+
 
 
 
